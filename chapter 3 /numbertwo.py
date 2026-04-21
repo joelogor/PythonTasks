509b621b3c4f03ac22ef95231663b884d9388eb5
joelogor <ogorjoelchukwuebuka@gmail.com>
2026-04-21 01:24:16 +0100

    chapter3

---
+++ chapter 3 /numbertwo.py
@@ -0,0 +1,6 @@
+number_one = int(input('Enter first integer:'))
+number_two = int(input('Enter second integer:'))
+
+sum = number_one + number_two
+
+print('The sum of' , number_one ,'and', number_two, 'is', sum)
